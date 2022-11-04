9391b718c25fbf8b5e71a0e10fb0a29ea397af30
Eva-Wamuyu <rutheve.eva@gmail.com>
2022-11-04 20:17:08 +0300

    .

--- app.py
+++ app.py
@@ -21,22 +21,24 @@ def calculate(data):
     try:
         x = data['x']
         y = data['y']
-        operation_type = data['operation_type']
+        operation_type = data['operation_type'].lower()
         operator = Operation[operation_type].value
-        print(type(Operation[operation_type].value))
+        print(type(Operation[operation_type]))
         if (operator =="*"):
             result = x * y
         elif (operator == "+"):
             result = x + y
         else:
             result = x - y
+            z = Operation.subtraction
         response_data = json.dumps(
         {"slackUsername": "Wamuyu",
         "result": result,
-        "operation_type": Operation[operation_type].value
+        "operation_type": data['operation_type']
         }
     )
     except KeyError:
+        
         return app.response_class(
             status=400,
             response={
@@ -44,6 +46,7 @@ def calculate(data):
             }
         )
     else:
+       
         response = app.response_class(
         response = response_data,
         status = 200,
@@ -53,9 +56,9 @@ def calculate(data):
 
     
 class Operation(str,Enum):
-    multiplication = '*',
-    addition = '+'
-    subtraction = '-'
+    multiplication = "*"
+    addition = "+"
+    subtraction = "-"
 
 if __name__=='__main__':
     app.run()
